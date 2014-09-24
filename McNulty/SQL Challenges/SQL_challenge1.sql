13ce77c1ab9046aa5b9bbe8a81e83c797e9cdeb4
jercoh <jejechaber289@msn.com>
2014-09-24 15:52:14 -0400

    fix SQL challenges

--- McNulty/SQL Challenges/SQL_challenge1.sql
+++ McNulty/SQL Challenges/SQL_challenge1.sql
@@ -3,34 +3,34 @@ FROM (
 		(
 		SELECT player1, SUM(count) AS totalcount
 		FROM (
-			SELECT player1, COUNT(*) AS count FROM us_women GROUP BY player1 
-			UNION 
-			SELECT player2, COUNT(*) AS count FROM us_women GROUP BY player2
+			SELECT player1, COUNT(*) AS count FROM us_men GROUP BY player1 
+			UNION ALL
+			SELECT player2, COUNT(*) AS count FROM us_men GROUP BY player2
 			) AS mytable 
 		GROUP BY player1 
 		ORDER BY SUM(count) DESC
 		)
-		UNION
+		UNION ALL
 		(
 		SELECT player1, SUM(count) AS totalcount
 		FROM (
-			SELECT player1, COUNT(*) AS count FROM french_women GROUP BY player1 
-			UNION 
-			SELECT player2, COUNT(*) AS count FROM french_women GROUP BY player2
+			SELECT player1, COUNT(*) AS count FROM french_men GROUP BY player1 
+			UNION ALL
+			SELECT player2, COUNT(*) AS count FROM french_men GROUP BY player2
 			) AS mytable1 
 		GROUP BY player1 
 		ORDER BY SUM(count) DESC
 		)
-		UNION
+		UNION ALL
 		(SELECT player1, SUM(count) AS totalcount
 		FROM (
-			SELECT player1, COUNT(*) AS count FROM aus_women GROUP BY player1 
-			UNION 
-			SELECT player2, COUNT(*) AS count FROM aus_women GROUP BY player2
+			SELECT player1, COUNT(*) AS count FROM aus_men GROUP BY player1 
+			UNION  ALL
+			SELECT player2, COUNT(*) AS count FROM aus_men GROUP BY player2
 			) AS mytable2
 		GROUP BY player1 
 		ORDER BY SUM(count) DESC
 		)
 ) AS tournaments
 GROUP BY player1 
-ORDER BY SUM(totalcount) DESC;
+ORDER BY SUM(totalcount);
